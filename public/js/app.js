ad7ad281e2dd77aad649feb4e63c28366dca08c9
JoySarkarBD <joysarkarbd407@gmail.com>
2022-11-17 11:50:08 +0600

    bulk selection and search data table added

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,16 @@
 const form = document.querySelector("#form");
 const tr = document.querySelectorAll("tbody tr");
 const addBtn = document.querySelector("#update");
+const bulk_check = document.querySelector("#bulk_check");
+const all_check_box = document.querySelectorAll(".check_box");
 
 /* update functionality */
-[...tr].forEach(tr => {
+[...tr].forEach((tr) => {
   tr.addEventListener("click", function (e) {
     if (this.dataset.id === e.target.dataset.id) {
       form.action = `/update/${this.dataset.id}`;
       addBtn.value = `Update`;
-      [...this.children].forEach(td => {
+      [...this.children].forEach((td) => {
         if (td.className === "word") {
           form[0].value = td.innerHTML;
         }
@@ -28,3 +30,19 @@ deleteBtn.addEventListener("click", function (e) {
   console.log(this.dataset.id);
 });
  */
+
+// bulk selection
+bulk_check.addEventListener("click", function (e) {
+  const checked = e.target.checked;
+  checkData = [];
+  if (checked) {
+    [...all_check_box].forEach((checkBox) => {
+      const tr = checkBox.parentElement.parentElement;
+      checkBox.checked = true;
+    });
+  } else {
+    [...all_check_box].forEach((checkBox) => {
+      checkBox.checked = false;
+    });
+  }
+});
